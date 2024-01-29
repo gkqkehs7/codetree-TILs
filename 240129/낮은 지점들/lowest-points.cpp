44b1e91cffb08d473bf56a9eb635a4d1cfeacf24
김민우 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-29 21:32:52 +0900

    Title: 낮은 지점들, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> hash;
+
+    for(int i=0; i<n; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        if(hash.find(x) != hash.end()) {
+            int origin_y = hash.find(x)->second;
+
+            cout << "find!\n";
+
+            if(y < origin_y) {
+                hash.erase(x);
+                hash.insert(make_pair(x, y));
+            }
+        } else {
+            hash.insert(make_pair(x, y));
+        }
+    }
+
+    int answer = 0;
+
+    for(auto elem : hash){
+       //  cout<<"key : "<<elem.first<<" value : "<<elem.second<<endl;
+        answer += elem.second;
+    }
+
+    cout << answer;
+}
