05e2bb19b9d28ed42a1ae79402da9b5bba30eea4
김민우 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-31 18:15:37 +0900

    Title: k번째로 작은 쌍의 합, Time: 109ms, Memory: 2MB, Status: Passed - CodeTree

--- 240131/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
+++ 240131/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
@@ -3,30 +3,38 @@
 #include <tuple>
 #include <algorithm>
 
+#define MAX_NUM 100000
+
 using namespace std;
 
+// 변수 선언
+int n, m, k;
+
+int arr1[MAX_NUM];
+int arr2[MAX_NUM];
+priority_queue<tuple<int, int, int> > pq;
+
 int main() {
-    int n, m, k;
+    // 입력:
+    cin >> n >> m >> k;
+    for(int i = 0; i < n; i++)
+        cin >> arr1[i];
 
-    int arr1[100000];
-    int arr2[100000];
+    for(int i = 0; i < m; i++)
+        cin >> arr2[i];
 
-    for(int i=0; i<n; i++) cin >> arr1[i];
-
-    for(int i=0; i<m; i++) cin >> arr2[i];
-
+    // 주어진 배열을 정렬해줍니다.
     sort(arr1, arr1 + n);
     sort(arr2, arr2 + m);
-
-    // 1 2 3 5 6 7
-    // 1 2 3 4
-
-    priority_queue<tuple<int, int, int>> pq;
-
-    for(int i = 0; i < n; i++) {
+    
+    // 처음에는 n개의 원소에 대해 각각 
+    // 두 번째 수열의 첫 번째 원소를 대응시켜줍니다.
+    // 작은 값이 더 먼저 나와야 하므로
+    // -를 붙여서 넣어줍니다. 
+    for(int i = 0; i < n; i++)
         pq.push(make_tuple(-(arr1[i] + arr2[0]), i, 0));
-    }
-
+    
+    // 1번부터 k - 1번까지 합이 작은 쌍을 골라줍니다.
     for(int i = 0; i < k - 1; i++) {
         int idx1, idx2;
         tie(ignore, idx1, idx2) = pq.top();
@@ -39,9 +47,10 @@ int main() {
             pq.push(make_tuple(-(arr1[idx1] + arr2[idx2]), idx1, idx2));
     }
 
+    // k번째 합을 가져옵니다.
     int pair_sum;
     tie(pair_sum, ignore, ignore) = pq.top();
-
+    
     cout << -pair_sum;
     return 0;
 }
