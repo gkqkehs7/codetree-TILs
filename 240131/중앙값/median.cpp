f7725a31ea6e3c1adfaf74515beaa6c023eb3ff0
김민우 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-31 17:16:12 +0900

    Title: 중앙값, Time: 27ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/중앙값/median.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int m;
+        cin >> m;
+
+        // 입력받는 부분
+        int arr[100000];
+        for(int i=0; i<m; i++) cin >> arr[i];
+
+        int mid = arr[0];
+        priority_queue<int> min_pq;
+        priority_queue<int> max_pq;
+
+        cout << mid << " ";
+
+        // mid 2  <- 5
+        // min 1 2
+        // max 3 4 5
+        for(int i=1; i<m; i++) {
+            if(min_pq.size() < max_pq.size()) {
+                if(mid < arr[i]) {
+                    min_pq.push(mid);
+
+                    max_pq.push(-arr[i]);
+
+                    mid = -max_pq.top();
+                    max_pq.pop();
+                } else if(mid > arr[i]) {
+                    min_pq.push(arr[i]);
+                } else if(mid == arr[i]) {
+                    min_pq.push(arr[i]);
+                }
+            } else if(min_pq.size() > max_pq.size()) {
+                if(mid < arr[i]) {
+                    max_pq.push(-arr[i]);
+                } else if(mid > arr[i]) {
+                    max_pq.push(mid);
+
+                    min_pq.push(arr[i]);
+
+                    mid = min_pq.top();
+                    
+                    min_pq.pop();
+                } else if(mid == arr[i]) {
+                    max_pq.push(-arr[i]);
+                }
+            } else if(min_pq.size() == max_pq.size()) {
+                if(mid < arr[i]) {
+                    max_pq.push(-arr[i]);
+                } else if(mid >= arr[i]) {
+                    min_pq.push(arr[i]);
+                }
+            }
+
+            if(i % 2 == 0) {
+                cout << mid << " ";
+            }
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
