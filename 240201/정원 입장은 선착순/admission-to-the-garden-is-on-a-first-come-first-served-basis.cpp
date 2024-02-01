576752bf9b987eac639d21afde1eb7ffad7dd86a
김민우 <77993709+gkqkehs7@users.noreply.github.com>
2024-02-01 15:01:39 +0900

    Title: 정원 입장은 선착순, Time: 6ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240201/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define INT_MAX 999999999
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<tuple<int, int, int>> peoples;
+    priority_queue<tuple<int, int, int>> pq;
+
+
+    for(int i=0; i<n; i++) {
+        int a, t;
+        cin >> a >> t;
+        peoples.push_back(make_tuple(a, i, t));
+    }
+
+    sort(peoples.begin(), peoples.end());
+
+    peoples[n] = make_tuple(INT_MAX, n + 1, 0);
+
+    // 첫번째 시작하는 사람의 끝나느 시각
+    int prev_end = 0;
+    int answer = 0;
+
+    for(int i=0; i<n; i++) {
+        int start_time, index, time;
+
+        tie(start_time, index, time) = peoples[i];
+
+        // 기다려야 되진 않지만 대기큐에 사람들이 있는 경우 (자기보다 앞선 index의 사람들이 있는 경우)
+        while(start_time > prev_end && !pq.empty()) {
+            int next_start_time, next_time;
+            tie(ignore, next_start_time, next_time) = pq.top();
+            pq.pop();
+
+            answer = max(answer, prev_end - next_start_time);
+            prev_end += next_time;
+        }
+
+        // 기다리지 않아도 되는 경우에는 종료 시간을 바로 늘려준다.
+        if(start_time > prev_end) {
+            prev_end = start_time + time;
+        } else { // 기다려야 한다면 대기큐에 넣어준다.
+            pq.push(make_tuple(-index, start_time, time));
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
